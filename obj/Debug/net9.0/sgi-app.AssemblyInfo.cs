1b0916f63f64e83237ac107eebc3ac07c1be751f
Fabian Galan <180666585+xkodigo1@users.noreply.github.com>
2025-05-09 18:41:48 -0500

    chore: :wastebasket: add .gitignore and remove ignored files

--- obj/Debug/net9.0/sgi-app.AssemblyInfo.cs
+++ obj/Debug/net9.0/sgi-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sgi-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68921c4332a019cd559192a304eab09e09f9557d")]
 [assembly: System.Reflection.AssemblyProductAttribute("sgi-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sgi-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
